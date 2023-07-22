7a28eb5b2ab63e961fce0779cb4b15a2acb71579
Thibault Lesuisse <lesuisse.thibault@gmail.com>
2023-07-22 11:43:19 +0200

    run format

--- src/deep-flat/deepflat.js
+++ src/deep-flat/deepflat.js
@@ -1,24 +1,23 @@
-/** 
-* Flattens nested arrays into single array
-* @param {Array} arr The array that needs flattening
-* @example 
-* // returns ['a', 'b', 'c']
-* deepflat(['a', ['b', ['c']]])
-* @returns {Array} the flattened array
-*/
-function deepflat(arr){
-    var result = [];
-    for(const elem of arr){
-        if(!Array.isArray(elem)){
-            result.push(elem)
-        }
-        else {
-            let flattened = deepflat(elem);
-            result.push(...flattened);
-        }
+/**
+ * Flattens nested arrays into single array
+ * @param {Array} arr The array that needs flattening
+ * @example
+ * // returns ['a', 'b', 'c']
+ * deepflat(['a', ['b', ['c']]])
+ * @returns {Array} the flattened array
+ */
+function deepflat(arr) {
+  var result = [];
+  for (const elem of arr) {
+    if (!Array.isArray(elem)) {
+      result.push(elem);
+    } else {
+      let flattened = deepflat(elem);
+      result.push(...flattened);
     }
+  }
 
-    return result;
+  return result;
 }
 
 export default deepflat;
