f8c87f03606622268563a2c70ea2f98d7e1985e3
Thibault Lesuisse <lesuisse.thibault@gmail.com>
2023-07-22 11:38:59 +0200

    add deepflat

---
+++ src/deep-flat/deepflat.js
@@ -0,0 +1,24 @@
+/** 
+* Flattens nested arrays into single array
+* @param {Array} arr The array that needs flattening
+* @example 
+* // returns ['a', 'b', 'c']
+* deepflat(['a', ['b', ['c']]])
+* @returns {Array} the flattened array
+*/
+function deepflat(arr){
+    var result = [];
+    for(const elem of arr){
+        if(!Array.isArray(elem)){
+            result.push(elem)
+        }
+        else {
+            let flattened = deepflat(elem);
+            result.push(...flattened);
+        }
+    }
+
+    return result;
+}
+
+export default deepflat;
